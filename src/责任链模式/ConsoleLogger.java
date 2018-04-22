596186dffda10c616c9999fdb74cfd0ac48a998a
weic <cw_bupt@163.com>
2018-04-22 23:29:54 +0800

    责任链模式实现

---
+++ src/责任链模式/ConsoleLogger.java
@@ -0,0 +1,11 @@
+package 责任链模式;
+
+public class ConsoleLogger extends AbstractLogger {
+
+    public ConsoleLogger(int level){
+        this.level=level;
+    }
+    protected void write(String message) {
+        System.out.println("Standary Console::Logger" + message);
+    }
+}
